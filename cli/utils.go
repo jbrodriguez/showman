05d95d8f75a3ed06210b60536a6a0f6625a51217
Juan B. Rodriguez <jbrodriguez@gmail.com>
2019-03-05 07:59:55 -0500

    Fix lint issues
    Ref #11

--- cli/utils.go
+++ cli/utils.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"io"
 	"os"
 	"path/filepath"
-	"sync"
 )
 
 // Exists - Check if File / Directory Exists
@@ -53,53 +50,53 @@ func IsDirEmpty(name string) (bool, error) {
 	return false, err
 }
 
-var lock sync.Mutex
+// var lock sync.Mutex
 
-// Save saves a representation of v to the file at path.
-func Save(path string, v interface{}) error {
-	lock.Lock()
-	defer lock.Unlock()
-	f, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	r, err := Marshal(v)
-	if err != nil {
-		return err
-	}
-	_, err = io.Copy(f, r)
-	return err
-}
+// // Save saves a representation of v to the file at path.
+// func Save(path string, v interface{}) error {
+// 	lock.Lock()
+// 	defer lock.Unlock()
+// 	f, err := os.Create(path)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer f.Close()
+// 	r, err := Marshal(v)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	_, err = io.Copy(f, r)
+// 	return err
+// }
 
-// Load loads the file at path into v.
-// Use os.IsNotExist() to see if the returned error is due
-// to the file being missing.
-func Load(path string, v interface{}) error {
-	lock.Lock()
-	defer lock.Unlock()
-	f, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	return Unmarshal(f, v)
-}
+// // Load loads the file at path into v.
+// // Use os.IsNotExist() to see if the returned error is due
+// // to the file being missing.
+// func Load(path string, v interface{}) error {
+// 	lock.Lock()
+// 	defer lock.Unlock()
+// 	f, err := os.Open(path)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer f.Close()
+// 	return Unmarshal(f, v)
+// }
 
-// Marshal is a function that marshals the object into an
-// io.Reader.
-// By default, it uses the JSON marshaller.
-var Marshal = func(v interface{}) (io.Reader, error) {
-	b, err := json.MarshalIndent(v, "", "\t")
-	if err != nil {
-		return nil, err
-	}
-	return bytes.NewReader(b), nil
-}
+// // Marshal is a function that marshals the object into an
+// // io.Reader.
+// // By default, it uses the JSON marshaller.
+// var Marshal = func(v interface{}) (io.Reader, error) {
+// 	b, err := json.MarshalIndent(v, "", "\t")
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return bytes.NewReader(b), nil
+// }
 
-// Unmarshal is a function that unmarshals the data from the
-// reader into the specified value.
-// By default, it uses the JSON unmarshaller.
-var Unmarshal = func(r io.Reader, v interface{}) error {
-	return json.NewDecoder(r).Decode(v)
-}
+// // Unmarshal is a function that unmarshals the data from the
+// // reader into the specified value.
+// // By default, it uses the JSON unmarshaller.
+// var Unmarshal = func(r io.Reader, v interface{}) error {
+// 	return json.NewDecoder(r).Decode(v)
+// }
